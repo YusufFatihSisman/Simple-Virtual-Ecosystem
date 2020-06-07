765434977b748ec2d4f5917b3496fe4ab157c408
Yusuf Fatih Şişman <62207089+YusufFatihSisman@users.noreply.github.com>
2020-06-07 19:48:43 +0300

    Update CreatureGenerator

--- CreatureGenerator.java
+++ CreatureGenerator.java
@@ -1,11 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 
 public class CreatureGenerator {
 	
-	// gender, age, gestationTime, weight, sppedLimit 
+	public static final int NUMBEROFGAZELLES = 100;
+	public static final int NUMBEROFCHEETAHS = 100;
+	
+	// gender, age(years), gestationTime(months), weight(kg), speedLimit(kmph)
 	public static void generateCreature(String fileName, int total, int minWeight, int maxWeight, int maxAgeFemale, int maxAgeMale, int gestation, int minSpeed, int maxSpeed){
 		
 		String gender, weight, age, speedLimit, gestationTime;
@@ -54,6 +60,36 @@ public class CreatureGenerator {
 		
 	}
 	
+	// Reading from file to ArrayList of ArrayList (Order: gender, age(years), gestationTime(months), weight(kg), speedLimit(kmph)) 
+	public static void read(ArrayList<ArrayList<String>> list, String fileName){
+			
+		String csvFile = fileName;
+	    BufferedReader br = null;
+	    String line = "";
+	    String temp = "";
+	    int j=0;
+	    
+	    try {
+	        br = new BufferedReader(new FileReader(csvFile));
+	        line = br.readLine();
+	        while ((line = br.readLine()) != null) {
+	        	list.add(new ArrayList<String>());
+	        	for(int i=0; i<line.length(); i++){
+	        		if(line.charAt(i) != ','){
+	        			temp += line.charAt(i);
+	        		}else{
+	        			list.get(j).add(temp);
+	        			temp = "";
+	        		}
+	        	}
+	        	j++;
+	        }
+	    } catch (IOException e) {
+	    	System.out.println(e);
+	    }
+	          
+	}  
+	
 	public static void main(String args[]){
 		
 		final int cheetahMinWeight = 20; //kg
@@ -72,9 +108,14 @@ public class CreatureGenerator {
 		final int GazelleMinSpeedLimit = 80; //kmph
 		final int GazelleMaxSpeedLimit = 97; //kmph
 		
-		generateCreature("cheetah.csv", 100, cheetahMinWeight, cheetahMaxWeight, cheetahMaxAgeFemale, cheetahMaxAgeMale, cheetahGestationTime, cheetahMinSpeedLimit, cheetahMaxSpeedLimit);
-		generateCreature("gazelle.csv", 100, GazelleMinWeight, GazelleMaxWeight, GazelleMaxAgeFemale, GazelleMaxAgeMale, GazelleGestationTime, GazelleMinSpeedLimit, GazelleMaxSpeedLimit);
-	
+		generateCreature("cheetah.csv", NUMBEROFCHEETAHS, cheetahMinWeight, cheetahMaxWeight, cheetahMaxAgeFemale, cheetahMaxAgeMale, cheetahGestationTime, cheetahMinSpeedLimit, cheetahMaxSpeedLimit);
+		generateCreature("gazelle.csv", NUMBEROFGAZELLES, GazelleMinWeight, GazelleMaxWeight, GazelleMaxAgeFemale, GazelleMaxAgeMale, GazelleGestationTime, GazelleMinSpeedLimit, GazelleMaxSpeedLimit);
+		
+		ArrayList<ArrayList<String>> GazelleList = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> CheetahList = new ArrayList<ArrayList<String>>();
+		
+		read(GazelleList, "gazelle.csv");
+		read(CheetahList, "cheetah.csv");
 	}
 	
 }
